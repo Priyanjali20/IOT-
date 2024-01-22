8741d6a1e518f7610a8682e5c4abe6e2f3e12697
Priyanjali20 <156687276+Priyanjali20@users.noreply.github.com>
2024-01-22 17:07:45 +0530

    Add files via upload

---
+++ sequenceGLOW_reverseLOW.ino
@@ -0,0 +1,19 @@
+// the setup function runs once when you press reset or power the board
+int i;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  for(i=6;i<=10;i++)
+  pinMode(i, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  for(i=10;i>=6;i--){
+  digitalWrite(i, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(500);  
+  digitalWrite(i,LOW);   // turn the LED off by making the voltage LOW
+  delay(500);                   // wait for a second
+  }
+}
+
+
